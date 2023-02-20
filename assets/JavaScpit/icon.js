5d45820cd13d6bf01a08d1ef21e30a64f94aec49
AssueroMota <assueromota@hotmail.com>
2023-02-20 11:54:07 -0300

    first commit

--- assets/JavaScpit/icon.js
+++ assets/JavaScpit/icon.js
@@ -1,9 +1,9 @@
 
-const Emailer = document.querySelector('.Emailer');
-const fa_regular = document.querySelector('.fa-regular');
-Emailer.addEventListener('mouseleave', () => {
-    Emailer.innerHTML = `<a href="#"><i class="fa-regular fa-envelope-open"></i><span>E-mail</span>`
-})
+// const Emailer = document.querySelector('.Emailer');
+// const fa_regular = document.querySelector('.fa-regular');
+// Emailer.addEventListener('mouseleave', () => {
+//     Emailer.innerHTML = `<a href="#"><i class="fa-regular fa-envelope-open"></i><span>E-mail</span>`
+// })
 
 // Emailer.addEventListener('mouseleave', () => {
 //     Emailer.innerHTML = `<a href="#"><i class="fa-regular fa-envelope"></i><span>E-mail</span>`
